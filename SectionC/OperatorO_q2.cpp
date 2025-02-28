ddfbdd7f7ae71755c239db8ea059ac8ce84f05e0
destroyedbyBrian <brian913@live.com>
2025-02-28 13:58:53 +0800

    Update OperatorO_q2.cpp

--- SectionC/OperatorO_q2.cpp
+++ SectionC/OperatorO_q2.cpp
@@ -36,25 +36,25 @@ int main() {
 
 // =============  RENDITION #2 ✅  =============
 class Vector2D {
-    public:
-        double x, y;
-        Vector2D () : x(0), y(0) {}
-        Vector2D (double x, double y) : x(x), y(y) {}
-    
-        Vector2D operator+(const Vector2D& other) {
-            return Vector2D(x + other.x, y + other.y);  // Use commas instead of logical operator
-        }
-        Vector2D operator-(const Vector2D& other) {
-            return Vector2D(x - other.x, y - other.y); // Use commas instead of logical operator
-        }
-    };
-    
-    int main() {
-        Vector2D v1(2, 2), v2(1, 3);
-        Vector2D v3 = v1 + v2;
-        Vector2D v4 = v1 - v3;
-    
-        std::cout << v3.x << ", " << v3.y << std::endl;
-        std::cout << v4.x << ", " << v4.y << std::endl;
-        return 0;
+public:
+    double x, y;
+    Vector2D () : x(0), y(0) {}
+    Vector2D (double x, double y) : x(x), y(y) {}
+
+    Vector2D operator+(const Vector2D& other) {
+        return Vector2D(x + other.x, y + other.y);  // Use commas instead of logical operator
     }
+    Vector2D operator-(const Vector2D& other) {
+        return Vector2D(x - other.x, y - other.y); // Use commas instead of logical operator
+    }
+};
+
+int main() {
+    Vector2D v1(2, 2), v2(1, 3);
+    Vector2D v3 = v1 + v2;
+    Vector2D v4 = v1 - v3;
+
+    std::cout << v3.x << ", " << v3.y << std::endl;
+    std::cout << v4.x << ", " << v4.y << std::endl;
+    return 0;
+}
